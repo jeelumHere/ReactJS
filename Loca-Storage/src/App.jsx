3bfeee2b19881bdc5375d2985b85a788a6da4af7
jeelumHere <syedsharjeelahmed214@gmail.com>
2026-04-04 01:06:37 +0500

    Local Storage Done

---
+++ Loca-Storage/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const App = () => {
+
+  // localStorage.setItem("user","sharjeel")
+  // const name = localStorage.getItem("user")
+  // console.log(name)
+  // localStorage.remove("user")
+  // localStorage.clear()
+
+  // let user = [{
+  //   name : "sharjeel ",
+  //   skill : "Problem Solver",
+  //   profession : "Frontend Developer",
+  //   age : "20",
+  //   libraray : "ReactJS"
+  // },{
+  //   name : "SOLVER ",
+  //   skill : "Problem Solver",
+  //   profession : "Seniour Frontend Developer",
+  //   age : "23",
+  //   libraray : "ReactJS & Next.JS"
+  // }]
+  // localStorage.setItem("user",JSON.stringify(user))
+
+  // let User = JSON.parse(localStorage.getItem("user"))
+
+  // console.log(User)
+
+  localStorage.clear()
+
+  return (
+    <div>
+      <h1>Learning Local Storage</h1>
+      
+    </div>
+  )
+}
+
+export default App
